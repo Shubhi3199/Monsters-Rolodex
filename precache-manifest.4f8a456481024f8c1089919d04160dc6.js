360ab7404bbfe59229e8df847a82a9ceb4f8247f
Shubhi3199 <44928935+Shubhi3199@users.noreply.github.com>
2019-10-26 09:56:39 +0530

    Updates

---
+++ precache-manifest.4f8a456481024f8c1089919d04160dc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcfbb257e915e2a4959ca3653f198944",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "3d85bebd328910f1df33",
+    "url": "/Monsters-Rolodex/static/css/main.e003c35b.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/Monsters-Rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "3d85bebd328910f1df33",
+    "url": "/Monsters-Rolodex/static/js/main.b8eb8f97.chunk.js"
+  },
+  {
+    "revision": "11e2dea3cac39d985a42",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.3ef3660b.js"
+  }
+]);
